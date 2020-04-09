1f5ed5aecf3532a52c59fd1e4bf0df889cf03ecc
Yash <2019peb5300@mnit.ac.in>
2020-04-10 05:24:25 +0530

    Create and use a session specific list

--- notes/app.py
+++ notes/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, session
+#more control over session. How they are stored server side
 from flask_session import Session
 
 app = Flask(__name__)
@@ -8,12 +9,16 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 #can create a global variable
-notes = []
+#notes = []
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    if session.get("notes") is None:
+    #if list is empty
+    #list specific to my session
+        session["notes"] = []
     if request.method == "POST":
         note = request.form.get("note")
-        notes.append(note)
+        session["notes"].append(note)
 
-    return render_template("index.html", notes=notes)
+    return render_template("index.html", notes=session["notes"])
