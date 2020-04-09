70f519a30a6bb553c9d46f6a1ddd967307e45122
Yash <2019peb5300@mnit.ac.in>
2020-04-09 05:52:16 +0530

    Set, dictionary and Loops

--- hello.py
+++ hello.py
@@ -17,3 +17,21 @@ else:
 coordinates = (10.0, 20.0)
 #list
 names = ['Alice', 'Bob', 'Charlie']
+#set  | no item shows up twice | orderless
+s = set()
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(2)
+print(s)
+#dictionary
+ages = {"Alice": 22, "Bob": 42}
+ages["Charlie"] = 30
+ages["Alice"] += 1
+print(ages)
+
+#loops
+for i in range(5):
+    print(i)
+for id, name in enumerate(names):
+    print(str(id) + name)
