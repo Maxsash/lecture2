af216d6d3c1263e156413a71ab0da668623c243e
Yash <2019peb5300@mnit.ac.in>
2020-04-09 05:41:09 +0530

    Basic use of if-else

--- hello.py
+++ hello.py
@@ -3,3 +3,12 @@ print('Hello, there!')
 #f denotes the use of format string
 name = input()
 print(f"Hey, {name} !")
+
+#conditions
+x=21
+if x>0:
+    print("x is positive")
+elif x<0:
+    print("x is negative")
+else:
+    print("x is zero")
