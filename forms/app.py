a2700afc36804281fd76a367bc3c1b6ea777c2e5
Yash <2019peb5300@mnit.ac.in>
2020-04-10 04:46:44 +0530

    Minor changes to form app.py

--- forms/app.py
+++ forms/app.py
@@ -7,8 +7,15 @@ def index():
     return render_template("index.html")
 
 #only accept POST reqeusts to the route /hello
-@app.route("/hello", methods=["POST"])
+#if try to directly access, we get "Method not allowed (GET is not allowed)"
+# @app.route("/hello", methods=["POST"])
+
+#accept GET request or POST requests
+@app.route("/hello", methods=["GET", "POST"])
 def hello():
+    if request.method == "GET":
+        return "Please submit the form instead"
 #name variable should be equal to value in name attribute in HTML form
-    name = request.form.get("name")
-    return render_template("hello.html", name=name)
+    else:
+        name = request.form.get("name")
+        return render_template("hello.html", name=name)
