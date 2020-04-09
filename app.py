f4505bbae2c00c1aa5861667adf08b0c4f390bf7
Yash <2019peb5300@mnit.ac.in>
2020-04-09 07:11:43 +0530

    Passing variable from python to HTML

--- app.py
+++ app.py
@@ -25,7 +25,14 @@ def maxsash():
 def hello(name):
     return f"<h1>What are you doing here, {name}??</h1>"
 
-##render_template
+#render_template
+#it will only look for hello.html inside the directory 'templates'
 @app.route("/index")
 def alto():
     return render_template("hello.html")
+
+@app.route("/variable")
+def head():
+    headline = "Forehead wrinkle is a headline"
+    return render_template("hello.html", headline=headline)
+#This will set the 'headline' variable inside hello.html equal to headline variable in this function
