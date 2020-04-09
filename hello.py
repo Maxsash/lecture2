6cd03d8c5bc3e5d88e994cd4c69bc56910f15e74
Yash <2019peb5300@mnit.ac.in>
2020-04-09 06:01:39 +0530

    create main function to avoid whole file running on external call of square

--- hello.py
+++ hello.py
@@ -1,43 +1,46 @@
-print('Hello, there!')
-
-#f denotes the use of format string
-name = input()
-print(f"Hey, {name} !")
-
-#conditions
-x=21
-if x>0:
-    print("x is positive")
-elif x<0:
-    print("x is negative")
-else:
-    print("x is zero")
-
-#tuple
-coordinates = (10.0, 20.0)
-#list
-names = ['Alice', 'Bob', 'Charlie']
-#set  | no item shows up twice | orderless
-s = set()
-s.add(1)
-s.add(2)
-s.add(3)
-s.add(2)
-print(s)
-#dictionary
-ages = {"Alice": 22, "Bob": 42}
-ages["Charlie"] = 30
-ages["Alice"] += 1
-print(ages)
-
-#loops
-for i in range(5):
-    print(i)
-for id, name in enumerate(names):
-    print(str(id) + name)
-
 #function
 def square(x):
     return x*x
-for i in range(10):
-    print("{} squared is {}".format(i, square(i)))
+
+def main():
+    print('Hello, there!')
+
+    #f denotes the use of format string
+    name = input()
+    print(f"Hey, {name} !")
+
+    #conditions
+    x=21
+    if x>0:
+        print("x is positive")
+    elif x<0:
+        print("x is negative")
+    else:
+        print("x is zero")
+
+    #tuple
+    coordinates = (10.0, 20.0)
+    #list
+    names = ['Alice', 'Bob', 'Charlie']
+    #set  | no item shows up twice | orderless
+    s = set()
+    s.add(1)
+    s.add(2)
+    s.add(3)
+    s.add(2)
+    print(s)
+    #dictionary
+    ages = {"Alice": 22, "Bob": 42}
+    ages["Charlie"] = 30
+    ages["Alice"] += 1
+    print(ages)
+
+    #loops
+    for i in range(5):
+        print(i)
+    for id, name in enumerate(names):
+        print(str(id) + name)
+
+    for i in range(10):
+        print("{} squared is {}".format(i, square(i)))
+
